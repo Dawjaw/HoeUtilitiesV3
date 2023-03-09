88ac040e4312bc93ca35bfbead3116a853805fb1
Dawjaw <battlefront43@gmail.com>
2023-03-09 08:51:19 +0100

    Update toolHud.js
    added missing constraint import

--- elementaHUD/toolHud.js
+++ elementaHUD/toolHud.js
@@ -11,6 +11,7 @@ import {
     SiblingConstraint,
     UIRoundedRectangle,
     UIWrappedText,
+    ChildBasedSizeConstraint,
     ChildBasedRangeConstraint,
     CenterConstraint,
 } from "../../Elementa";
